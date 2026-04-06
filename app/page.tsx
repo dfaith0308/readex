4cc9b39ba01057dee4ea067ef0caf2a9796cb3d8
dfaith0308 <88176751+dfaith0308@users.noreply.github.com>
2026-04-06 10:41:28 +0900

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,3 @@
-// app/page.tsx
-import { redirect } from 'next/navigation'
-import { createClient } from '@/lib/supabase-server'
-
-export default async function RootPage() {
-  const supabase = createClient()
-  const {
-    data: { user },
-  } = await supabase.auth.getUser()
-
-  if (user) {
-    redirect('/home')
-  } else {
-    redirect('/login')
-  }
+export default function Page() {
+  return <div>TEST SUCCESS</div>
 }
